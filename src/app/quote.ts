b8b0c314a1f9eab60fca681b3f055b8d366a7d91
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-16 16:55:13 +0300

    Added a quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    id: number;
+    name: string;
+    quote: string;
+
 }
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quote:Quote[] = [
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
